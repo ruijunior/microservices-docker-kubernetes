4f9409f872294e4517efd24f28991e5760fb0c99
Rui Junior <junior_rui@yahoo.com.brl>
2022-04-12 23:42:49 -0400

    ::Endpoints course::

--- course/src/main/java/com/ead/course/services/CourseService.java
+++ course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,9 +1,21 @@
 package com.ead.course.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import com.ead.course.models.CourseModel;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CourseService {
 
     void delete(CourseModel course);
+
+    CourseModel save(CourseModel courseModel);
+
+    Optional<CourseModel> findById(UUID courseId);
+
+    Page<CourseModel> findAll(Pageable pageable);
     
 }
--- course/src/main/java/com/ead/course/services/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/CourseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ead.course.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
@@ -12,6 +14,8 @@ import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -41,6 +45,21 @@ public class CourseServiceImpl implements CourseService{
             moduleRepository.deleteAll(modules);
         }
         courseRepository.delete(course);
+    }
+
+    @Override
+    public CourseModel save(CourseModel courseModel) {
+        return courseRepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public Page<CourseModel> findAll(Pageable pageable) {
+        return courseRepository.findAll(pageable);
     }
     
 }
